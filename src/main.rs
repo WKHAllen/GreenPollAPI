5731e7205c1d772ed9e1eaf7180be50c66937876
WKHAllen <wkhallen@gmail.com>
2021-04-28 10:53:59 -0500

    Use Actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{HttpResponse, Result, get};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    message: String,
+}
+
+#[get("/")]
+async fn index() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().json(Message {
+        message: String::from("Hello, world!")
+    }))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    use actix_web::{App, HttpServer};
+
+    let server = HttpServer::new(|| App::new().service(index))
+        .bind("localhost:3000")?
+        .run();
+
+    println!("Server running at http://localhost:3000/");
+
+    server.await
 }
