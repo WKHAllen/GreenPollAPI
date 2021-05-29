7f44cb9df6e5366ac40fc4a3f6909183bd9a0fe0
WKHAllen <wkhallen@gmail.com>
2021-05-29 10:05:42 -0400

    Order poll options and votes by ID

--- sql/poll/get_poll_options.sql
+++ sql/poll/get_poll_options.sql
@@ -1,1 +1,1 @@
-SELECT * FROM poll_option WHERE poll_id = $1;
+SELECT * FROM poll_option WHERE poll_id = $1 ORDER BY id;
--- sql/poll/get_poll_votes.sql
+++ sql/poll/get_poll_votes.sql
@@ -1,1 +1,1 @@
-SELECT * FROM poll_vote WHERE poll_id = $1;
+SELECT * FROM poll_vote WHERE poll_id = $1 ORDER BY id;
